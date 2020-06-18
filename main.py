28d547eae7f66c5514b804258a48a75f7af1a1d5
JShimonov <jshimonov@gmail.com>
2020-06-17 21:59:10 -0400

    initialized

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+gamertag = input('what is your gamer tag: ')
+
+url = "https://call-of-duty-modern-warfare.p.rapidapi.com/multiplayer/" + gamertag[:gamertag.find('#')] + '%2523' + gamertag[gamertag.find('#')+1:] + "/battle"
+print(url)
+
+headers = {
+    'x-rapidapi-host': "call-of-duty-modern-warfare.p.rapidapi.com",
+    'x-rapidapi-key': os.getenv('API_KEY')
+}
+
+response = requests.request("GET", url, headers=headers)
+
+print(response.text)
